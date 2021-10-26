960c81f9d4c839eede9dc8bd4160008e2077a0bb
Avery <asmith0902@gmail.com>
2021-10-25 20:23:43 -0400

    update

--- mail.php
+++ mail.php
@@ -1,17 +1,58 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "asmith0902@gmail.com";
-$subject = "Mail From My Website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@averysmithwebsite.com" . "\r\n" .
-"";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-    header("Location:thankyou.html");
-}
-//redirect
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-?>
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//get data from form
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// preparing mail content
+$messagecontent ="Name = ". $name . "<br>Email = " . $email . "<br>Message =" . $message;
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = '9c0a483cc71a6e';                     //SMTP username
+    $mail->Password   = '29b63a4a46f3c4';                               //SMTP password
+   // $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('from@averysmithwebsite.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+   // $mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('photo.jpeg', 'photo.jpeg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = $messagecontent;
+    
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+   // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
